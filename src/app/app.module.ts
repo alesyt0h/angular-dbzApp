09cfc9e5bb6cde9d0448c6128fead71ad569e8ea
Alex Ortigosa <alexortigosa179@gmail.com>
2021-07-16 20:42:39 +0200

    DBZ App Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { ContadorModule } from './contador/contador.module'
-import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -12,8 +11,7 @@ import { HeroesModule } from './heroes/heroes.module';
   ],
   imports: [
     BrowserModule,
-    ContadorModule,
-    HeroesModule
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
